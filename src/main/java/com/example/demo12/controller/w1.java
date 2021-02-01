c04ac5050536f101a086cf4e923352b332fd5b36
qwerewt <39213577+qwerewt@users.noreply.github.com>
2021-02-01 22:58:19 +0800

    Update w1.java

--- src/main/java/com/example/demo12/controller/w1.java
+++ src/main/java/com/example/demo12/controller/w1.java
@@ -3,6 +3,7 @@ package com.example.demo12.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * @Auther:http://www.bjsxt.com
